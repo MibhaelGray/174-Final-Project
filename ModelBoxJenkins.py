00633d95eb5ab8b0e768c3b77f02a2c8a039ca60
MibhaelGray <mgray.gray4173@gmail.com>
2025-03-01 15:35:45 -0800

    Yay

--- ModelBoxJenkins.py
+++ ModelBoxJenkins.py
@@ -9,47 +9,24 @@ from sklearn.metrics import mean_absolute_error
 import warnings
 
 
-monthly_data = pd.read_csv("Data/Time-Scaled Data/monthly_LMP.csv", parse_dates=['Local Date'])
-monthly_data.set_index('Local Date')
-time_series = monthly_data['North LMP']
-adf_result = adfuller(time_series)
+HB_NORTH_DATA = pd.read_csv("Final Project/Data/ERCOT SPPS/All_HB_NORTH_Data.csv")
 
-# Plotting the time series data
 
-plt.figure(figsize=(12,5))
-plt.plot(monthly_data.index, monthly_data['North LMP'], marker='o', linestyle='-')
-plt.xlabel("Time (Months)")
-plt.ylabel("Dollars per megawatt-hour ($/MWh).")
-plt.title("Dallas-Fort Worth Wholesale Energy Prices")
-plt.grid()
+
+
+
+# Convert Delivery Date and Hour into a proper datetime format
+HB_NORTH_DATA['Datetime'] = pd.to_datetime(HB_NORTH_DATA['Delivery Date']) + pd.to_timedelta(HB_NORTH_DATA['Delivery Hour'] - 1, unit='h')
+
+# Set Datetime as the index
+HB_NORTH_DATA.set_index('Datetime', inplace=True)
+
+# Plot the Settlement Point Price over time
+plt.figure(figsize=(12, 6))
+plt.plot(HB_NORTH_DATA.index, HB_NORTH_DATA['Settlement Point Price'], label='Settlement Point Price', color='b', alpha=0.7)
+plt.xlabel('Time')
+plt.ylabel('Price ($)')
+plt.title('ERCOT HB NORTH Settlement Point Price Over Time')
+plt.legend()
+plt.grid(True)
 plt.show()
-
-
-# Print results
-print("ADF Statistic:", adf_result[0])
-print("p-value:", adf_result[1])
-print("Critical Values:")
-
-if adf_result[1] < 0.05:
-    print("The series is stationary (reject null hypothesis).")
-else:
-    print("The series is non-stationary")
-
-# Now let's create some ACF and PACF plots to determine if the data 
-
-# Plot ACF
-plt.figure(figsize=(12, 5))
-plot_acf(time_series.dropna(), lags=60)
-plt.title("Autocorrelation Function (ACF)")
-plt.ylabel('Correlation (-1,1)')
-plt.show()
-
-# Plot PACF
-plt.figure(figsize=(12, 5))
-plot_pacf(time_series.dropna(), lags=30)
-plt.title("Partial Autocorrelation Function (PACF)")
-plt.ylabel('Correlation (-1,1)')
-plt.show()
-
-
-
